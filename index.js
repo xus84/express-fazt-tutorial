4576a47a689f9728d24e9c5cb2d94609747287fa
g_garsan@hotmail.com <g_garsan@hotmail.com>
2022-08-29 18:31:16 +0200

    ES6 modules

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('hello')
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/',(req,res)=>{
+    res.send('this is homepage')
+})
+
+app.listen( port ,()=>{
+    console.log('server is running at port number 3000')
+});
